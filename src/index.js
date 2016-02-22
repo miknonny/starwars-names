6514084537a0990245a85414152b4b007a77cbe0
nonnycode <miknonny@gmail.com>
2016-02-22 15:36:04 +0100

    Adding the library

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var starwarsNames = require('./starwars-names.json')
+var uniqueRandomArray = require('unique-random-array')
+
+module.exports = {
+  all: starwarsNames,
+  random: uniqueRandomArray(starwarsNames)
+}
